7ae0f6b7479674fb7fe53bfef6b89fe9ba7714e6
Philipp Bauer <pbauer@klerk.io>
2018-06-24 13:21:08 -0500

    Define target browsers for Babel

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,13 @@ let config = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/env'],
+            presets: [
+              ["@babel/env", {
+                "targets": {
+                  "browsers": ["last 2 versions"]
+                }
+              }]
+            ],
             plugins: ['@babel/plugin-transform-runtime']
           }
         }
