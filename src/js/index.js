69cfdd1b044db09522d9f7e8ea1292fca2b8baec
Philipp Bauer <hello@philippbauer.org>
2018-07-17 16:48:29 -0500

    Roll instantiation into the onload event.

--- src/js/index.js
+++ src/js/index.js
@@ -9,20 +9,18 @@ import FetchController from "./controllers/FetchController";
 import RawController from "./controllers/RawController";
 import UrlController from "./controllers/UrlController";
 
-let flash = new Flash();
-let progress = new Progress();
-let preview = new Preview();
-
 // Let's start the controllers
 // when the page finished loading.
 window.onload = function() {
+  let flash = new Flash();
+  let progress = new Progress();
+  let preview = new Preview();
+
   let fetchForm = new Form("#fetchLocker");
   let rawForm = new Form("#storeLocker");
   let urlForm = new Form("#urlLocker");
 
-  let fetchController = new FetchController(fetchForm, preview, flash, progress);
-  let rawController = new RawController(rawForm, fetchForm, preview, flash, progress);
-  let urlController = new UrlController(urlForm, fetchForm, preview, flash, progress);
-
-  // console.log(fetchController, rawController, urlController);
+  new FetchController(fetchForm, preview, flash, progress);
+  new RawController(rawForm, fetchForm, preview, flash, progress);
+  new UrlController(urlForm, fetchForm, preview, flash, progress);
 }
